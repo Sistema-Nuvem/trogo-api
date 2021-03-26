ea0e25b0ecf8105d799f6bdf57f158df441bfbb2
jairpro <jairedias@yahoo.com.br>
2021-03-25 21:13:02 -0300

    Corrige sugestão do dia do mês
    Problema: No dia 25/03/2021 Entrie gerava expiration para o dia 4 quando dia do vencimento na Account é null
    
    Solução:  substituir moment().day() para moment().date()

--- src/util/moment.ts
+++ src/util/moment.ts
@@ -9,8 +9,20 @@ export interface OptionsData {
 export function dateFix(options: OptionsData, format: string = 'YYYY-MM-DD') {
   const year = options.year ?? moment().year()
   const month = options.month ?? moment().month()
+
+  /*const test = {
+    day: moment().day(), // retorna o mesmo que weekday (exemplo para quinta-feira, retorna 4)
+    days: moment().days(),
+    daysInMonth: moment().daysInMonth(),
+    weekday: moment().weekday(),
+    isoweekday: moment().isoWeekday(),
+    dayOfYear: moment().dayOfYear(),
+    date: moment().date(), // Esse retorna o dia do mês exemplo 25/03/2021, retorna 25
+    toDate: moment().toDate(),
+  }*/
+
   const day = Math.min(
-    Number(options.day ?? moment().day()), 
+    Number(options.day ?? moment().date()), 
     moment({
       year,
       month,
